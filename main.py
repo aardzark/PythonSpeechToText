a9068c34cce106343eed210c991dc450d0fbdef6
Aardzark <Aardzarks@gmail.com>
2022-11-03 05:39:45 -0400

    Add two clean UA audio samples, tabulate info to console, refactor

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from deepspeech import Model
+from tabulate import tabulate
 import numpy as np
 import wave
 
 # robinhad/voice-recognition-ua
 # pre-trained language model on 1230 hours of Ukrainian
-UKRAINIAN_MODEL_ROBINHAD = "Neural Network Models/ukrainian_robinhad.pbmm"
+UKRAINIAN_MODEL_ROBINHAD = "Neural Network Models/Ukrainian/DeepSpeech/robinhad_voice-recognition-ua-v-0-04/uk.pbmm"
 # outside language model that assists with transcription
-UKRAINIAN_SCORER_ROBINHAD = "Neural Network Models/kenlm_robinhad.scorer"
+UKRAINIAN_SCORER_ROBINHAD = "Neural Network Models/Ukrainian/DeepSpeech/robinhad_voice-recognition-ua-v-0-04/kenlm.scorer"
 
-UKRAINIAN_AUDIO_SAMPLE = "Audio Sample/ukrainian_demo.wav"
+# github.com/egorsmkv/speech-recognition-uk/tree/master/tts-demos
+UKRAINIAN_AUDIO_SAMPLE_NEON = "Audio Sample/tts-demos_neon_tts.wav"
+UKRAINIAN_AUDIO_SAMPLE_SILERO = "Audio Sample/tts-demos_silero_tts.wav"
+EXPECTED_OUTPUT_UKRAINIAN_AUDIO_SAMPLE = """Кам'янець-Подільський - місто в Хмельницькій області України, центр Кам'янець-Подільської
+міської об'єднаної територіальної громади і Кам'янець-Подільського району."""
 
 
-def setup_deepspeech(model_path,ukrainian_config):
+def setup_deepspeech(model_path, ukrainian_config):
     beam_width = 100
 
     # value listed on release page for model
@@ -48,5 +53,15 @@ if __name__ == '__main__':
         "lm_alpha": "0.7200873732640549",
         "lm_beta": "1.3010463457623596"
     }
+
     ukrainian_model = setup_deepspeech(UKRAINIAN_MODEL_ROBINHAD, ukrainian_config)
-    print(transcribe_batch(ukrainian_model, UKRAINIAN_AUDIO_SAMPLE))
+    tabular_data = [['DeepSpeech', 'robinhad v0.4', 'tts-demos_neon_tts.wav', 'Ukrainian',
+                     transcribe_batch(ukrainian_model, UKRAINIAN_AUDIO_SAMPLE_NEON),
+                     EXPECTED_OUTPUT_UKRAINIAN_AUDIO_SAMPLE],
+                    ['DeepSpeech', 'robinhad v0.4', 'tts-demos_silero_tts.wav', 'Ukrainian',
+                     transcribe_batch(ukrainian_model, UKRAINIAN_AUDIO_SAMPLE_SILERO),
+                     EXPECTED_OUTPUT_UKRAINIAN_AUDIO_SAMPLE]]
+
+    print(tabulate(tabular_data,
+                   headers=['Engine', 'Model', 'File', 'Language', 'Actual Speech-to-Text', 'Expected Speech-to-Text'],
+                   tablefmt="fancy_grid"))
